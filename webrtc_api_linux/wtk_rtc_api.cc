db8f1ae65073510c170ff0788507dd535423dd52
Xiaofan@Desktop <xiaofan.lee@wattertek.com>
2018-06-15 18:19:16 +0800

    Add video + audio test, still error

--- webrtc_api_linux/wtk_rtc_api.cc
+++ webrtc_api_linux/wtk_rtc_api.cc
@@ -1,244 +1,262 @@
 #include "wtk_rtc_api.h"
 
+#define USED_AUDIO_DEVICE_ID 0
+#define USED_VIDEO_DEVICE_ID 0
 
-//webrtc::cricket::WebRtcVoiceEngine* g_VoiceEngine = nullptr;
-rtc::scoped_refptr<webrtc::AudioDeviceModule> g_adm = nullptr;
-rtc::scoped_refptr<webrtc::AudioProcessing> g_apm = nullptr;
-rtc::scoped_refptr<webrtc::AudioMixerImpl> g_amm = nullptr;
+#define USED_VIDEO_WIDTH 640
+#define USED_VIDEO_HEIGHT 480
+#define USED_VIDEO_FPS 30
 
-std::unique_ptr<webrtc::RtcEventLog> event_log = nullptr;
+rtc::scoped_refptr<webrtc::AudioDeviceModule> g_Adm = nullptr;
+std::unique_ptr<webrtc::RtcEventLog> g_Event_log = nullptr;
 
-/*webrtc::AudioDeviceModule* g_adm = nullptr;
-webrtc::AudioProcessing* g_apm = nullptr;
-webrtc::AudioMixerImpl* g_amm = nullptr;*/
-webrtc::AudioCodingModule* g_acm = nullptr;
-webrtc::Call* g_call = nullptr;
+webrtc::Call* g_Call = nullptr;
 
-webrtc::AudioSendStream *g_audioSendStream = nullptr;
-webrtc::AudioReceiveStream *g_audioReceiveStream = nullptr;
+webrtc::AudioSendStream *g_AudioSendStream = nullptr;
+webrtc::AudioReceiveStream *g_AudioReceiveStream = nullptr;
+webrtc::VideoSendStream *g_VideoSendStream = nullptr;
+webrtc::VideoReceiveStream *g_VideoReceiveStream = nullptr;
 
-#define AUDIO_SEND_SSRC		1111
-#define AUDIO_REC_SSRC		2222
+std::unique_ptr<webrtc::test::VideoRenderer> local_render = nullptr;
+std::unique_ptr<webrtc::test::VideoRenderer> remote_render = nullptr;
+
+enum classPayloadTypes{
+    kPayloadTypeIlbc = 102,
+    kPayloadTypeOpus = 111,
+    kPayloadTypeH264 = 122,
+    kPayloadTypeVP8 = 120,
+    kPayloadTypeVP9 = 101,
+};
 
 class AudioLoopTransport:public webrtc::Transport{
 public:
     bool SendRtp(const uint8_t* packet,size_t length,const webrtc::PacketOptions& options) override
-    {
-		RTC_LOG(INFO) << __FUNCTION__ << " =============================>start";
-		
+    {		
         int64_t send_time = webrtc::Clock::GetRealTimeClock()->TimeInMicroseconds();
-        if(g_call != nullptr){
-	        webrtc::PacketReceiver::DeliveryStatus status = webrtc::PacketReceiver::DeliveryStatus::DELIVERY_OK;
-			rtc::CopyOnWriteBuffer buffer(packet, length);
-			webrtc::PacketReceiver* rev = g_call->Receiver();
-			rev->DeliverPacket(webrtc::MediaType::AUDIO, buffer, webrtc::PacketTime(send_time, -1));
-			
-	        assert(status == webrtc::PacketReceiver::DeliveryStatus::DELIVERY_OK);
-	        RTC_LOG(INFO) << __FUNCTION__ << " =============================>status = " << status ;
-    	}
+		g_Call->Receiver()->DeliverPacket(webrtc::MediaType::AUDIO, rtc::CopyOnWriteBuffer(packet, length), webrtc::PacketTime(send_time, -1));
+		
         return true;
     }
 
     bool SendRtcp(const uint8_t* packet, size_t length) override
     {
-		RTC_LOG(INFO) << __FUNCTION__ << " =============================>start";
-		//int64_t send_time = webrtc::Clock::GetRealTimeClock()->TimeInMicroseconds();
-        webrtc::PacketReceiver::DeliveryStatus status = g_call->Receiver()->DeliverPacket(webrtc::MediaType::AUDIO, 
-																					        rtc::CopyOnWriteBuffer(packet, length), 
-																					        webrtc::PacketTime());
-        assert(status == webrtc::PacketReceiver::DeliveryStatus::DELIVERY_OK);
-        RTC_LOG(INFO) << __FUNCTION__ << " =============================>status = " << status ;
+		int64_t send_time = webrtc::Clock::GetRealTimeClock()->TimeInMicroseconds();
+        g_Call->Receiver()->DeliverPacket(webrtc::MediaType::AUDIO, rtc::CopyOnWriteBuffer(packet, length), webrtc::PacketTime(send_time, -1));
+
         return true;
     }
 };
-AudioLoopTransport* g_audioSendTransport = nullptr;
+class VideoLoopTransport:public webrtc::Transport{
+public:
+    bool SendRtp(const uint8_t* packet,size_t length,const webrtc::PacketOptions& options) override
+    {		
+        int64_t send_time = webrtc::Clock::GetRealTimeClock()->TimeInMicroseconds();
+		g_Call->Receiver()->DeliverPacket(webrtc::MediaType::VIDEO, rtc::CopyOnWriteBuffer(packet, length), webrtc::PacketTime(send_time, -1));
+		
+        return true;
+    }
+
+    bool SendRtcp(const uint8_t* packet, size_t length) override
+    {
+		int64_t send_time = webrtc::Clock::GetRealTimeClock()->TimeInMicroseconds();
+        g_Call->Receiver()->DeliverPacket(webrtc::MediaType::VIDEO, rtc::CopyOnWriteBuffer(packet, length), webrtc::PacketTime(send_time, -1));
+
+        return true;
+    }
+};
+class TestVideoEncoderFactory:public webrtc::VideoEncoderFactory {
+    std::vector<webrtc::SdpVideoFormat> GetSupportedFormats() const override;
+
+    CodecInfo QueryVideoEncoder(const webrtc::SdpVideoFormat& format) const override;
+
+    std::unique_ptr<webrtc::VideoEncoder> CreateVideoEncoder(
+        const webrtc::SdpVideoFormat& format) override;
+private:
+    webrtc::InternalEncoderFactory internal_encoder_factory_;
+};
+
+AudioLoopTransport* g_AudioSendTransport = nullptr;
+VideoLoopTransport* g_VideoSendTransport = nullptr;
+
+rtc::scoped_refptr<webrtc::AudioEncoderFactory> CreateWtkAudioEncoderFactory() {
+  return webrtc::CreateAudioEncoderFactory<
+#if WTK_USE_BUILTIN_OPUS
+      webrtc::AudioEncoderOpus,
+#endif
+#if WTK_USE_BUILTIN_ILBC
+      webrtc::AudioEncoderIlbc
+#endif
+>();
+}
+rtc::scoped_refptr<webrtc::AudioDecoderFactory> CreateWtkAudioDecoderFactory() {
+  return webrtc::CreateAudioDecoderFactory<
+#if WTK_USE_BUILTIN_OPUS
+      webrtc::AudioDecoderOpus,
+#endif
+#if WTK_USE_BUILTIN_ILBC
+      webrtc::AudioDecoderIlbc
+#endif
+>();
+}
 
 void CreateAudioSendStream(void)
 {
-	RTC_LOG(INFO) << __FUNCTION__ << " =============================>start";
-	g_audioSendTransport = new AudioLoopTransport();
-    webrtc::AudioSendStream::Config config(g_audioSendTransport);
-	config.rtp.ssrc = AUDIO_SEND_SSRC;
-	//config.min_bitrate_bps = 100*1000;
-	//config.max_bitrate_bps = 500*1000;
-	config.send_codec_spec = webrtc::AudioSendStream::Config::SendCodecSpec(102, {"ILBC", 8000, 1});
-	config.encoder_factory = webrtc::CreateBuiltinAudioEncoderFactory();
+	g_AudioSendTransport = new AudioLoopTransport();
+    webrtc::AudioSendStream::Config config(g_AudioSendTransport);
+	config.send_codec_spec = webrtc::AudioSendStream::Config::SendCodecSpec(kPayloadTypeOpus, {"OPUS", 48000, 2});
+	config.encoder_factory = CreateWtkAudioEncoderFactory();
 
-    g_audioSendStream = g_call->CreateAudioSendStream(config);
-	
-
-    assert(g_audioSendStream);
-	RTC_LOG(INFO) << __FUNCTION__ << " =============================>ended";
+    g_AudioSendStream = g_Call->CreateAudioSendStream(config);
 }
-
 void CreateAudioReceiveStream(void)
 {
-	RTC_LOG(INFO) << __FUNCTION__ << " =============================>start";
-
-	rtc::scoped_refptr<webrtc::AudioDecoderFactory> audioDecoderFactory = webrtc::CreateBuiltinAudioDecoderFactory();
+	rtc::scoped_refptr<webrtc::AudioDecoderFactory> audioDecoderFactory = CreateWtkAudioDecoderFactory();
     webrtc::AudioReceiveStream::Config config;
+	config.rtcp_send_transport = g_AudioSendTransport;
     config.decoder_factory = audioDecoderFactory;
-	config.rtp.remote_ssrc = AUDIO_SEND_SSRC;
-	config.rtp.local_ssrc = AUDIO_REC_SSRC;
-	config.decoder_map = {{102, {"ILBC", 8000, 1}}};
+	config.sync_group = "AudioVideoLoopbackTest";
+	config.decoder_map = {{kPayloadTypeOpus, {"OPUS", 48000, 2}}};
 
-    g_audioReceiveStream = g_call->CreateAudioReceiveStream(config);
-
-    assert(g_audioReceiveStream);
-	RTC_LOG(INFO) << __FUNCTION__ << " =============================>ended";
+    g_AudioReceiveStream = g_Call->CreateAudioReceiveStream(config);
 }
 
-void StartCall(void)
+void CreateVideoSendStream(void)
 {
-	RTC_LOG(INFO) << __FUNCTION__ << " =============================>start";
-	g_call->SignalChannelNetworkState(webrtc::MediaType::AUDIO, webrtc::kNetworkUp);
-
-    g_audioSendStream->Start();
-    g_audioReceiveStream->Start();
-	RTC_LOG(INFO) << __FUNCTION__ << " =============================>ended";
-}
-
-void CallApi_Setup(void)
-{
-	RTC_LOG(INFO) << __FUNCTION__ << " =============================>start";
+	g_VideoSendTransport = new VideoLoopTransport();
+    webrtc::VideoSendStream::Config send_config(g_VideoSendTransport);
+	/*webrtc::VideoEncoderFactory* encoder_factory;
+	webrtc::SdpVideoFormat video_format;
+	video_format.name = "VP8";
+	encoder_factory->CreateVideoEncoder(video_format);
+	for (webrtc::SdpVideoFormat& format : encoder_factory->GetSupportedFormats()) {
+		RTC_LOG(INFO) << __FUNCTION__ << ", format name = " << format.name;
+	}*/
+	TestVideoEncoderFactory g_video_encoder_factory;
+	//g_video_encoder_factory = new TestVideoEncoderFactory();
+	send_config.encoder_settings.encoder_factory = &g_video_encoder_factory;
+	send_config.rtp.payload_name = "VP8";
+	send_config.rtp.payload_type = kPayloadTypeVP8;
 	
+	webrtc::VideoEncoderConfig encoder_config;
+	encoder_config.codec_type = webrtc::kVideoCodecVP8;
+	encoder_config.number_of_streams = 1;
+	encoder_config.max_bitrate_bps = 100000;
+	encoder_config.simulcast_layers = std::vector<webrtc::VideoStream>(1);
+	encoder_config.content_type = webrtc::VideoEncoderConfig::ContentType::kRealtimeVideo;
+
+    g_VideoSendStream = g_Call->CreateVideoSendStream(std::move(send_config), std::move(encoder_config));
+}
+void CreateVideoReceiveStream(void)
+{
+    webrtc::VideoReceiveStream::Config config(g_VideoSendTransport);
+	config.renderer = remote_render.get();
+    config.sync_group = "AudioVideoLoopbackTest";
+
+	webrtc::VideoReceiveStream::Decoder decoder;
+	decoder.payload_name = "VP8";
+	decoder.payload_type = kPayloadTypeVP8;
+	config.decoders.push_back(decoder);
+
+    g_VideoReceiveStream = g_Call->CreateVideoReceiveStream(std::move(config));
+}
+
+void SetupLocalRender(void)
+{
+	local_render.reset(webrtc::test::VideoRenderer::Create("Video Local Render", USED_VIDEO_WIDTH, USED_VIDEO_HEIGHT));
+}
+
+void SetupRomoteRender(void)
+{
+	remote_render.reset(webrtc::test::VideoRenderer::Create("Video Remote Render", USED_VIDEO_WIDTH, USED_VIDEO_HEIGHT));
+}
+
+void StartCapture(void)
+{
+	std::unique_ptr<webrtc::VideoCaptureModule::DeviceInfo> device_info(webrtc::VideoCaptureFactory::CreateDeviceInfo());
+
+	char device_name[256]={0};
+    char unique_name[256]={0};
+	//int number_of_devices = device_info->NumberOfDevices();
+
+	device_info->GetDeviceName(USED_VIDEO_DEVICE_ID, device_name, sizeof(device_name), unique_name, sizeof(unique_name));
+
+	rtc::scoped_refptr<webrtc::VideoCaptureModule> device_module(webrtc::VideoCaptureFactory::Create(unique_name));
+	
+	webrtc::VideoCaptureCapability capability;
+	capability.width = USED_VIDEO_WIDTH;
+	capability.height = USED_VIDEO_HEIGHT;
+	capability.maxFPS = USED_VIDEO_FPS;
+	capability.videoType = webrtc::VideoType::kUnknown;
+
+	device_module->StartCapture(capability);
+}
+
+void StartCallTest(void)
+{
+	g_Call->SignalChannelNetworkState(webrtc::MediaType::AUDIO, webrtc::kNetworkUp);
+	g_Call->SignalChannelNetworkState(webrtc::MediaType::VIDEO, webrtc::kNetworkUp);
+
+    g_AudioSendStream->Start();
+    g_AudioReceiveStream->Start();
+	g_VideoSendStream->Start();
+    g_VideoReceiveStream->Start();
+}
+
+void CreateCall(void)
+{
 	webrtc::AudioState::Config audioStateConfig;
-	audioStateConfig.audio_device_module = g_adm;
-	audioStateConfig.audio_mixer = g_amm;
-	audioStateConfig.audio_processing = g_apm;
+	audioStateConfig.audio_device_module = g_Adm;
+	audioStateConfig.audio_mixer = webrtc::AudioMixerImpl::Create();
+	audioStateConfig.audio_processing = webrtc::AudioProcessingBuilder().Create();
 	
-	event_log = webrtc::RtcEventLog::Create(webrtc::RtcEventLog::EncodingType::Legacy);
-	webrtc::CallConfig callConfig(event_log.get());
-
+	g_Event_log = webrtc::RtcEventLog::Create(webrtc::RtcEventLog::EncodingType::Legacy);
+	webrtc::CallConfig callConfig(g_Event_log.get());
 	callConfig.audio_state = webrtc::AudioState::Create(audioStateConfig);
-	//callConfig.bitrate_config.max_bitrate_bps = 500*1000;
-	//callConfig.bitrate_config.min_bitrate_bps = 100*1000;
-	//callConfig.bitrate_config.start_bitrate_bps = 250*1000;
+	g_Adm->RegisterAudioCallback(callConfig.audio_state->audio_transport());
 
-	g_call = webrtc::Call::Create(callConfig);
+	g_Call = webrtc::Call::Create(callConfig);
+}
 
-	g_adm->RegisterAudioCallback(callConfig.audio_state->audio_transport());
+void AudioDeviceModule_Setup(void)
+{	
+	bool stereo_playout = false;
+	bool stereo_record = false;
+
+	if(!g_Adm)
+		g_Adm = webrtc::AudioDeviceModule::Create(webrtc::AudioDeviceModule::kPlatformDefaultAudio);
+	g_Adm->Init();
+	g_Adm->SetPlayoutDevice(USED_AUDIO_DEVICE_ID);
+	g_Adm->InitSpeaker();
+	g_Adm->SetRecordingDevice(USED_AUDIO_DEVICE_ID);
+	g_Adm->InitMicrophone();
 	
-	assert(g_call);
-
-	RTC_LOG(INFO) << __FUNCTION__ << " =============================>ended";
+	g_Adm->StereoPlayoutIsAvailable(&stereo_playout);
+	g_Adm->SetStereoPlayout(stereo_playout);
+	g_Adm->StereoRecordingIsAvailable(&stereo_record);
+	g_Adm->SetStereoRecording(stereo_record);
+	g_Adm->InitPlayout();
+	g_Adm->InitRecording();
 }
 
-void Amm_Setup(void)
-{
-	RTC_LOG(INFO) << __FUNCTION__ << " =============================>start";
-
-	if(!g_amm)
-		g_amm =  webrtc::AudioMixerImpl::Create();
-	RTC_LOG(INFO) << __FUNCTION__ << " =============================>ended";
-}
-void Apm_Setup(void)
-{
-	RTC_LOG(INFO) << __FUNCTION__ << " =============================>start";
-
-	if(!g_apm)
-		g_apm = webrtc::AudioProcessingBuilder().Create();
-
-	RTC_LOG(INFO) << __FUNCTION__ << " =============================>ended";
-}
-void Adm_Setup(void)
-{
-	RTC_LOG(INFO) << __FUNCTION__ << " =============================>start";
-	
-	if(!g_adm)
-		g_adm = webrtc::AudioDeviceModule::Create(webrtc::AudioDeviceModule::kLinuxAlsaAudio);//kPlatformDefaultAudio
-	//g_adm->Init();
-	
-	webrtc::AudioDeviceModule::AudioLayer audio_layer;
-	int got_platform_audio_layer = g_adm->ActiveAudioLayer(&audio_layer);
-	bool requirements_satisfied_ = true;
-	bool stereo_playout_ = false;
-	// First, ensure that a valid audio layer can be activated.
-	if (got_platform_audio_layer != 0) {
-		requirements_satisfied_ = false;
-	}
-	// Next, verify that the ADM can be initialized.
-	if (requirements_satisfied_) {
-		requirements_satisfied_ = (g_adm->Init() == 0);
-	}
-	// Finally, ensure that at least one valid device exists in each direction.
-#define USED_DEVICE_ID 0
-    if (requirements_satisfied_) {
-		if (g_adm->SetPlayoutDevice(USED_DEVICE_ID) != 0) {
-		    RTC_LOG(INFO) << __FUNCTION__ << ", SetPlayoutDevice error";
-		}
-		if (g_adm->InitSpeaker() != 0) {
-		    RTC_LOG(INFO) << __FUNCTION__ << ", InitSpeaker error";
-		}
-		if (g_adm->SetRecordingDevice(USED_DEVICE_ID) != 0) {
-		    RTC_LOG(INFO) << __FUNCTION__ << ", SetRecordingDevice error";
-		}
-		if (g_adm->InitMicrophone() != 0) {
-		    RTC_LOG(INFO) << __FUNCTION__ << ", InitMicrophone error";
-		}
-		if (g_adm->StereoPlayoutIsAvailable(&stereo_playout_) != 0) {
-		    RTC_LOG(INFO) << __FUNCTION__ << ", StereoPlayoutIsAvailable error";
-		}
-		if (g_adm->SetStereoPlayout(stereo_playout_) != 0) {
-		    RTC_LOG(INFO) << __FUNCTION__ << ", SetStereoPlayout error";
-		}
-		if (g_adm->SetStereoRecording(false) != 0) {
-		    RTC_LOG(INFO) << __FUNCTION__ << ", SetStereoRecording error";
-		}
-
-		if (g_adm->InitPlayout() != 0) {
-		    RTC_LOG(INFO) << __FUNCTION__ << ", InitPlayout error";
-		}
-		/*if (g_adm->StartPlayout() != 0) {
-			RTC_LOG(INFO) << __FUNCTION__ << ", StartPlayout error";
-		}*/
-		// start record
-		if (g_adm->InitRecording() != 0) {
-		    RTC_LOG(INFO) << __FUNCTION__ << ", InitRecording error";
-		}
-		/*if (g_adm->StartRecording() != 0) {
-			RTC_LOG(INFO) << __FUNCTION__ << ", StartRecording error";
-		}*/
-    }
-	//while(1);
-	RTC_LOG(INFO) << __FUNCTION__ << " =============================>ended";
-}
-void Acm_Setup(void)
-{
-	RTC_LOG(INFO) << __FUNCTION__ << " =============================>start";
-
-	webrtc::CodecInst dummyCodec;
-	webrtc::CodecInst usedCodec;
-	if(!g_acm)
-			g_acm = webrtc::AudioCodingModule::Create(webrtc::AudioCodingModule::Config(webrtc::CreateBuiltinAudioDecoderFactory()));
-
-	uint8_t numCodecs = g_acm->NumberOfCodecs();
-	for (uint8_t n = 0; n < numCodecs; n++) {
-		g_acm->Codec(n, &dummyCodec);
-		if (STR_CASE_CMP(dummyCodec.plname, "ILBC") == 0)
-		{
-			usedCodec = dummyCodec;
-		}
-	}
-	g_acm->RegisterSendCodec(usedCodec);
-	g_acm->RegisterReceiveCodec(usedCodec);
-	
-	RTC_LOG(INFO) << __FUNCTION__ << " used coded: " << usedCodec;
-	RTC_LOG(INFO) << __FUNCTION__ << " =============================>ended";
-}
 int main(void)
 {
-	RTC_LOG(INFO) << "++++++++++++++++++++Test main start+++++++++++++++++++++++";
-	//Acm_Setup();
-	Adm_Setup();
-	Apm_Setup();
-	Amm_Setup();
+	AudioDeviceModule_Setup();
 	
-	CallApi_Setup();
+	CreateCall();
+
 	CreateAudioSendStream();
 	CreateAudioReceiveStream();
-	StartCall();
+
+	SetupLocalRender();
+	SetupLocalRender();
+	StartCapture();	
+	
+	CreateVideoSendStream();
+	CreateVideoReceiveStream();
+	
+	StartCallTest();
+	
 	while(1);
-	RTC_LOG(INFO) << "++++++++++++++++++++Test main ended+++++++++++++++++++++++";
+
 	return 0;
 }
--- webrtc_api_linux/wtk_rtc_api.h
+++ webrtc_api_linux/wtk_rtc_api.h
@@ -3,22 +3,38 @@
 
 #include <memory>
 #include <utility>
+#include <vector>
+#include <string>
 
 
-// org mode
+#define WTK_USE_BUILTIN_OPUS 1
+#define WTK_USE_BUILTIN_ILBC 1
+
 #include "rtc_base/logging.h"
 #include "logging/rtc_event_log/rtc_event_log.h"
-#include "modules/audio_coding/include/audio_coding_module.h"
 #include "modules/audio_device/include/audio_device.h"
 #include "modules/audio_processing/include/audio_processing.h"
 #include "modules/audio_mixer/audio_mixer_impl.h"
-#include "api/audio_codecs/builtin_audio_decoder_factory.h"
-#include "api/audio_codecs/builtin_audio_encoder_factory.h"
+#include "modules/video_capture/video_capture.h"
+#include "modules/video_capture/video_capture_factory.h"
+
+#include "api/video_codecs/sdp_video_format.h"
+
+#include "api/audio_codecs/audio_encoder_factory_template.h"
+#include "api/audio_codecs/audio_decoder_factory_template.h"
+#if WTK_USE_BUILTIN_OPUS
+#include "api/audio_codecs/opus/audio_encoder_opus.h"
+#include "api/audio_codecs/opus/audio_decoder_opus.h"
+#endif
+#if WTK_USE_BUILTIN_ILBC
+#include "api/audio_codecs/ilbc/audio_encoder_ilbc.h"
+#include "api/audio_codecs/ilbc/audio_decoder_ilbc.h"
+#endif
+#include "media/engine/internalencoderfactory.h"
 
 // call api mode
 #include "call/call.h"
 #include "system_wrappers/include/clock.h"
-
-#include "media/engine/webrtcvoiceengine.h"
+#include "test/video_renderer.h"
 
 #endif
